f01fbb7d5fa9164ae7a3486a8a8da656c254c967
leticea <nascimento.leticeah@gmail.com>
2022-11-26 11:53:33 -0300

    criando função para buscar os meses

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+    const tableDays = document.getElementById('days');
 
     function getDaysCalendar(month, year) {
         document.getElementById('month').innerHTML = months[month];
         document.getElementById('year').innerHTML = year;
+
+        let firstDayOfWeek = new Date(year, month, 1).getDay()-1;
+        let getLastDayThisMonth = new Date(year, month+1, 0).getDate();
+
+        for(let i = -firstDayOfWeek; i < (42-firstDayOfWeek); i++) {
+            let date = new Date(year, month, i);
+        }
     }
     getDaysCalendar(2, 2001);
 });
