fa45c4aeff2c4f6a43101ddbed22ab2d2a73a4dc
leticea <nascimento.leticeah@gmail.com>
2022-11-26 11:37:05 -0300

    criando função para buscar os meses

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+    function getDaysCalendar(month, year) {
+        console.log(month);
+    }
+    getDaysCalendar(2, 2001);
+});
